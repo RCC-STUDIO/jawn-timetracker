908cb367d6d0dcd98d99c89fd6f50924c6ad2051
RCC-STUDIO <rccstudio.clarksor@gmail.com>
2024-01-19 17:26:35 -0700

    Update TermsOfService.tsx

---
+++ components/TermsOfService.tsx
@@ -0,0 +1,15 @@
+
+
+export default function TermsOfService() {
+    //Display Terms of Service when creating an account
+    return (
+        <div>
+            <h1>Terms of Service</h1>
+            <div>
+                <p>Place Holder</p>
+            </div>
+            <button>Accept</button>
+            <button>Decline</button>
+        </div>
+    )
+}
