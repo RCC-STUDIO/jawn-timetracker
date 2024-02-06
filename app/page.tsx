73fe1b4b1d7441b8db5ef6b507824cd2ba03b797
Matt <147024601+matthew-simms-298@users.noreply.github.com>
2024-02-06 09:46:44 -0700

    modal changes made, can't find it

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
 import NavBar from "@/components/NavBar"
-
+import Modal from "@react-modal/libs/components/Modal";
 export default function Home() {
 
+  const openModal = () => {
+
+  }
+  
   type Shift = {
     id: number,
     shiftType: string,
@@ -37,19 +41,19 @@ export default function Home() {
       <div className="flex flex-col items-center bg-slate-800 border-w w-1/2 p-5 rounded-lg">
         {shifts.map((shift) => (
           <div className="flex flex-row border-2 rounded-lg w-full text-left p-4 m-2" key={shift.id}>
-            <div className="flex flex-col w-3/4">
+            <div className="flex flex-col w-3/4"
+            onChange={() => openModal()}>
               <h2>Type: {shift.shiftType}</h2>
               <p>Day Of Week: {shift.dayOfWeek}</p>
               <p>Department: {shift.department}</p>
             </div>
-            <div className="w-1/4 flex justify-center items-center">
-              <button className="hover:bg-red-700 rounded-lg p-5 m-1 border">
-                Drop
-              </button>
-              <button className="hover:bg-blue-700 rounded-lg p-5 m-1 border">
-                Swap
-              </button>
-            </div>
+            <Modal>
+              <div>
+                <p>
+                  testing
+                </p>
+              </div>
+            </Modal>
           </div>
         ))}
       </div>
