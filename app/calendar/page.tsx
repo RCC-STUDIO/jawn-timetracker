b2301ae9853b3c49464fb94b8fe15d724f67255f
Matt <147024601+matthew-simms-298@users.noreply.github.com>
2024-02-15 16:46:40 -0700

    lots of work done
    completed the layout for the Google Pixel 7's
    
    - Profile screen GUI adapted
    - Main screen GUI adapted
    - Currently working on the GUI for the master week schedule screen
    > dummy data will be used temporarily while i set it up
    > I need to figure out how to map everything
    > we need to work on file imports

--- app/calendar/page.tsx
+++ app/calendar/page.tsx
@@ -1,10 +1,40 @@
 
+
+const fakeSchedule = [
+    ["", "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"],
+    ["Ryan C.", "", "8:00 AM - 4:00 PM", "8:00 AM - 4:00 PM", "8:00 AM - 4:00 PM", "8:00 AM - 4:00 PM", "8:00 AM - 4:00 PM", ""],
+    ["Matthew S.", "", "9:00 AM - 5:00 PM", "9:00 AM - 5:00 PM", "9:00 AM - 5:00 PM", "9:00 AM - 5:00 PM", "9:00 AM - 5:00 PM", ""],
+    ["Ben R.", "", "10:00 AM - 6:00 PM", "10:00 AM - 6:00 PM", "10:00 AM - 6:00 PM", "", "", ""],
+    ["Cyril D.", "", "1:00 PM - 9:00 PM", "", "", "", "", ""],
+    ["Gabriel L.", "", "12:00 PM - 8:00 PM", "12:00 PM - 8:00 PM", "", "", "", ""]
+]
+
 export default function CalendarPage() {
     return (
-        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <main className="flex min-h-screen flex-col items-center justify-between">
             <div className="flex flex-col">
-                <h1 className=" text-3xl underline m-3 p-3 text-center">Calendar</h1>
-                <img className=" p-3 border-2 border-white rounded-2xl" src="./images/def_real_calendar.png" alt="def real calendar"></img>
+                <h1 className=" text-3xl underline p-10 text-center">Calendar</h1>
+                <table className="table-auto overflow-scroll w-full p-1">
+                    <tbody className="">
+                        <tr className="">
+                            {fakeSchedule[0].map((jndex) => {
+                                return (
+                                    <td className="border border-white text-xs p-1">{jndex}</td>
+                                )
+                            })}
+                        </tr>
+                        {fakeSchedule.map((index) => {
+                            return (
+                                index.map((jndex) => {
+                                    <tr>
+                                        
+                                    </tr>
+                                })
+                            )
+                        })}
+                            
+                    </tbody>
+                </table>
             </div>
         </main>
     );
