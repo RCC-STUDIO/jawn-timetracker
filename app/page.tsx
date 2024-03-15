7b44d473fd076853ee6aa1e215f0f74fefbc8032
Gabriel Leclerc <gleclerc.yk1@gmail.com>
2024-03-15 15:45:19 -0600

    Refactored content around the Shift List

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,20 @@ import ShiftList from '@/components/ShiftsList';
 export default function Home() {  
   
   return (
-      <ShiftList />
+    <main className="flex flex-col items-center justify-between py-10 px-5">
+        <h1 className="p-5 font-bold text-2xl">My Shifts</h1>
+        <div className="flex flex-col items-center bg-blue-950 w-full p-5 rounded-lg">
+          <ShiftList />
+        </div>
+  
+      {/* {modalState && (
+        <div className="modal-overlay flex flex-col bg-blue-950 p-4 border border-blue-950 rounded-md w-full items-center m-2">
+          <div className="flex justify-end w-full mb-2">
+            <button onClick={() => setModalState(false)} className="bg-red-500 p-2 border rounded-lg border-red-500">Cancel</button>
+          </div>
+        <ShiftRequest/>
+      </div> )
+      } */}
+    </main>
   )
 }
--- components/ShiftsList.tsx
+++ components/ShiftsList.tsx
@@ -1,5 +1,3 @@
-import ShiftRequest from "./modals/ShiftRequest";
-
 const getShifts = async () => {
     try {
         const response = await fetch('http://localhost:3000/api/shifts', {
@@ -21,10 +19,8 @@ export default async function ShiftList() {
     const shiftStyle = "flex flex row justify-between";
     
     return (
-      <main className="flex flex-col items-center justify-between py-10 px-5">
-        <h1 className="p-5 font-bold text-2xl">My Shifts</h1>
-        <div className="flex flex-col items-center bg-blue-950 w-full p-5 rounded-lg">
-          {shifts.map((shift: any) => (
+        <>
+            {shifts.map((shift: any) => (
             <div className="flex flex-row border-2 rounded-lg w-full text-left p-4 m-2 hover:bg-white hover:border-blue-950 hover:text-blue-950" key={shift.id}>
               <div className="flex flex-col w-full">
                 <div className={shiftStyle}>
@@ -47,17 +43,7 @@ export default async function ShiftList() {
             </div>
              
           ))}
-        </div>
-  
-        {/* {modalState && (
-          <div className="modal-overlay flex flex-col bg-blue-950 p-4 border border-blue-950 rounded-md w-full items-center m-2">
-            <div className="flex justify-end w-full mb-2">
-              <button onClick={() => setModalState(false)} className="bg-red-500 p-2 border rounded-lg border-red-500">Cancel</button>
-            </div>
-          <ShiftRequest/>
-        </div> )
-        } */}
-      </main>
+        </>
     )
   }
   
