f8148c356d968a06041c2f7686631b9c538af670
Gabriel Leclerc <119021693+GabrielPJL12@users.noreply.github.com>
2024-03-15 19:36:37 -0600

    Indentation

--- components/ShiftsList.tsx
+++ components/ShiftsList.tsx
@@ -19,32 +19,32 @@ export default async function ShiftList() {
     const shiftStyle = "flex flex row justify-between";
     
     return (
-        <>
-            {await getShifts().then(shifts => (
-              shifts.map((shift: any) => (
-                <div className="flex flex-row border-2 rounded-lg w-full text-left p-4 m-2 hover:bg-white hover:border-blue-950 hover:text-blue-950" key={shift._id}>
-                  <div className="flex flex-col w-full">
-                    <div className={shiftStyle}>
-                        <h2>Start Date:</h2>
-                        <p>{shift.startDate}</p>
-                    </div>
-                    <div className={shiftStyle}>
-                      <h2>End Date:</h2>
-                      <p>{shift.endDate}</p>
-                    </div>
-                    <div className={shiftStyle}>
-                      <h2>Employee:</h2>
-                      <p>{shift.employee}</p>
-                    </div>
-                    <div className={shiftStyle}>
-                      <h2>Department:</h2>
-                      <p>{shift.department}</p>
-                    </div>
-                  </div>
+      <>
+        {await getShifts().then(shifts => (
+          shifts.map((shift: any) => (
+            <div className="flex flex-row border-2 rounded-lg w-full text-left p-4 m-2 hover:bg-white hover:border-blue-950 hover:text-blue-950" key={shift._id}>
+              <div className="flex flex-col w-full">
+                <div className={shiftStyle}>
+                  <h2>Start Date:</h2>
+                  <p>{shift.startDate}</p>
                 </div>
-            ))
-          ))}
-        </>
+                <div className={shiftStyle}>
+                  <h2>End Date:</h2>
+                  <p>{shift.endDate}</p>
+                </div>
+                <div className={shiftStyle}>
+                  <h2>Employee:</h2>
+                  <p>{shift.employee}</p>
+                </div>
+                <div className={shiftStyle}>
+                  <h2>Department:</h2>
+                  <p>{shift.department}</p>
+                </div>
+              </div>
+            </div>
+          ))
+        ))}
+      </>
     )
   }
   
