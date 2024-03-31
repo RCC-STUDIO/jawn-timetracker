e549bda60b1ec228bbfff3c02dc764e5087f715f
Matt <147024601+matthew-simms-298@users.noreply.github.com>
2024-03-30 23:56:59 -0600

    Fixed client and server side clashes

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,8 @@
-'use client'
-import { useState } from "react";
 import NavBar from "@/components/NavBar";
 import ShiftRequest from "@/components/ShiftRequest";
 import ShiftList from "@/components/ShiftsList";
 
 export default function Home() {
-  const [modalState, setModalState] = useState(-1); // Initialize modal state with -1
 
   const shiftStyle = "flex flex-row justify-between";
   type Shift = {
@@ -36,16 +33,11 @@ export default function Home() {
     },
   ];
 
-  const toggleModal = (shiftId: number) => {
-    // Toggle the modal state for the clicked shift
-    setModalState((previous) => (previous === shiftId ? -1 : shiftId));
-  };
-
   return (
     <main className="min-h-screen flex flex-col items-center justify-between py-10 px-5">
       <h1 className="p-5 font-bold text-2xl">My Shifts</h1>
       <div className="flex flex-col items-center bg-blue-950 w-full p-5 rounded-lg">
-        <ShiftList />
+        <ShiftList/>
       </div>
     </main>
   );
--- components/ShiftsList.tsx
+++ components/ShiftsList.tsx
@@ -1,39 +1,75 @@
+'use client'
 import { getShifts } from "@/libs/dbAccess";
+import { useState, useEffect } from "react";
+import ShiftRequest from "./ShiftRequest";
 
-export default async function ShiftList() {
-    const shiftStyle = "flex flex row justify-between";
-    
-    return (
-      <>
-        {await getShifts().then(shifts => (
-          shifts.map((shift: any) => (
-            <div className="flex flex-row border-2 rounded-lg w-full text-left p-4 m-2 hover:bg-white hover:border-blue-950 hover:text-blue-950" key={shift._id}>
-              <div className="flex flex-col w-full">
-                <div className={shiftStyle}>
-                  <h2>Start Date:</h2>
-                  <p>{shift.startDate}</p>
-                </div>
-                <div className={shiftStyle}>
-                  <h2>End Date:</h2>
-                  <p>{shift.endDate}</p>
-                </div>
-                <div className={shiftStyle}>
-                  <h2>Employee:</h2>
-                  <p>{shift.employee_id}</p>
-                </div>
-                <div className={shiftStyle}>
-                  <h2>Department:</h2>
-                  <p>{shift.department_id}</p>
-                </div>
-                <div className={shiftStyle}>
-                  <h2>Status:</h2>
-                  <p>{shift.status}</p>
-                </div>
+interface Shift {
+  startDate: string;
+  endDate: string;
+  employee_id: string;
+  department_id: string;
+  status: string;
+  _id: string;
+}
+
+
+
+
+export default function ShiftList() {
+  const shiftStyle = "flex flex row justify-between";
+  const [shifts, setShifts] = useState<Shift[]>([]);
+  const [modalState, setModalState] = useState(-1);
+
+  useEffect(() => {
+    async function fetchShifts() {
+      try {
+        const shiftsData = await getShifts();
+        // the user "65fb91a6733b75969216cb2d" is being used for testing purposes
+        const userShifts = shiftsData.filter((shift: Shift) => shift.employee_id === "65fb91a6733b75969216cb2d");
+
+        setShifts(userShifts);
+      } catch (error) {
+        console.error("Error fetching shifts:", error);
+      }
+    }
+    fetchShifts();
+  }, []);
+
+  const toggleModal = (key: number) => {
+    setModalState((previous) => (previous === key ? -1 : key));
+  };
+
+  return (
+    <>
+      {shifts.map((shift, key) => (
+        <div className="flex flex-row border-2 rounded-lg w-full text-left p-4 m-2" key={shift._id}>
+          <div>
+            <div onClick={() => toggleModal(key)} className="flex flex-col w-full">
+              <div className={shiftStyle}>
+                <h2>Start Date:</h2>
+                <p>{shift.startDate}</p>
+              </div>
+              <div className={shiftStyle}>
+                <h2>End Date:</h2>
+                <p>{shift.endDate}</p>
+              </div>
+              <div className={shiftStyle}>
+                <h2>Employee:</h2>
+                <p>{shift.employee_id}</p>
+              </div>
+              <div className={shiftStyle}>
+                <h2>Department:</h2>
+                <p>{shift.department_id}</p>
+              </div>
+              <div className={shiftStyle}>
+                <h2>Status:</h2>
+                <p>{shift.status}</p>
               </div>
             </div>
-          ))
-        ))}
-      </>
-    )
-  }
-  
+            {modalState === key && <ShiftRequest />}
+          </div>
+        </div>
+      ))}
+    </>
+  );
+}
