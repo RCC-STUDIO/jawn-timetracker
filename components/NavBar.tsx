b307f97a1ad6862553ccb5ffb555c6c26129c3b8
RCC-STUDIO <rccstudio.clarksor@gmail.com>
2024-04-01 10:47:18 -0600

    Update NavBar Styling

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -4,6 +4,7 @@ import { useSession } from "next-auth/react"
 
 //Styles Variables
 const buttonStyle = "bg-blue-950 hover:bg-blue-700 hover:text-blue-950 text-white p-5 w-full text-center"
+const imageStyles = "w-10 h-10 m-auto"
 let loggedIn = true
 
 export default function NavBar() {
@@ -20,20 +21,20 @@ export default function NavBar() {
           }
     }
     return (
-        loggedIn && (<nav className="flex flex-row text-xl text-white w-full">
+        loggedIn && (<nav className="flex flex-row text-xl text-white w-full justify-center items-center text-center">
                 <div className={buttonStyle}>
                     <a href="/calendar">
-                        <img src="\images\calendar-button.png" alt="Calendar"/>
+                        <img className={imageStyles} src="\images\calendar-button.png" alt="Calendar"/>
                     </a>
                 </div>
                 <div className={buttonStyle}>
                     <a href="/">
-                        <img src="\images\home-button.png" alt="Home"/>
+                        <img className={imageStyles} src="\images\home-button.png" alt="Home"/>
                     </a>
                 </div>
                 <div className={buttonStyle}>
                     <a href="/profile">
-                        <img src="\images\profile_icon.jpg" alt="Profile"/>
+                        <img className={imageStyles} src="\images\profile_icon.jpg" alt="Profile"/>
                     </a>
                 </div>
             
