23ca00c00583721227132227df948b9c22e65818
Matt <147024601+matthew-simms-298@users.noreply.github.com>
2024-04-02 12:20:05 -0600

    Database mostly connected

--- app/page.tsx
+++ app/page.tsx
@@ -1,38 +1,6 @@
-import NavBar from "@/components/NavBar";
-import ShiftRequest from "@/components/ShiftRequest";
 import ShiftList from "@/components/ShiftsList";
 
 export default function Home() {
-
-  const shiftStyle = "flex flex-row justify-between";
-  type Shift = {
-    id: number;
-    shiftType: string;
-    dayOfWeek: string;
-    department: string;
-  };
-
-  const shifts: Shift[] = [
-    {
-      id: 1,
-      shiftType: "Close",
-      dayOfWeek: "Monday",
-      department: "Produce",
-    },
-    {
-      id: 2,
-      shiftType: "Midday",
-      dayOfWeek: "Thursday",
-      department: "Produce",
-    },
-    {
-      id: 3,
-      shiftType: "Open",
-      dayOfWeek: "Saturday",
-      department: "Produce",
-    },
-  ];
-
   return (
     <main className="min-h-screen flex flex-col items-center justify-between py-10 px-5">
       <h1 className="p-5 font-bold text-2xl">My Shifts</h1>
--- components/SwapRequests.tsx
+++ components/SwapRequests.tsx
@@ -1,6 +1,18 @@
 import React from "react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import SwapOptions from "./SwapOptions";
+import { getRequest } from "@/libs/dbAccess";
+import { setRequestMeta } from "next/dist/server/request-meta";
+
+interface Request {
+  firstShift_id: string;
+  secondShift_id: string;
+  requester_id: string;
+  requestee_id: string;
+  department_id: string;
+  status: string;
+}
+
 // STYLE CONSTANTS
 const requestStyles =
   "bg-blue-100 p-3 w-full items-center rounded-md text-black";
@@ -13,34 +25,50 @@ const fakeSwapRequests = [
 
 export default function SwapRequests() {
   const [modalState, setModalState] = useState(-1);
+  const [requests, setRequests] = useState<Request[]>([]);
   const toggleModal = (shiftId: number) => {
     // Toggle the modal state for the clicked shift
     setModalState((previous) => (previous === shiftId ? -1 : shiftId));
   };
 
+  useEffect(() => {
+    async function fetchShifts() {
+      try {
+        const shiftsData = await getRequest();
+        // the user "65fb91a6733b75969216cb2d" is being used for testing purposes
+        const userRequests = shiftsData.filter((request: Request) => request.requestee_id === "65fb91a6733b75969216cb2d");
+
+        setRequests(userRequests);
+      } catch (error) {
+        console.error("Error fetching shifts:", error);
+      }
+    }
+    fetchShifts();
+  }, []);
+
   return (
     <div className="w-full bg-blue-950 p-5 mt-7 border rounded-md border-blue-950">
       <h2 className="text-center font-bold">SWAP REQUESTS</h2>
-      {fakeSwapRequests.map((index, key) => (
+      {requests.map((request, key) => (
         <div className="bg-blue-100 border rounded-md m-3">
         <div onClick={() => toggleModal(key)} key={key} className={requestStyles}>
           <div className="">
             <div className={requestContentStyles}>
               <p>Employee:</p>
-              <p>{index[0]}</p>
+              <p>{request.requester_id}</p>
             </div>
             <div className={requestContentStyles}>
               <p>Theirs:</p>
               <div className="flex flex-col text-right">
-                <p>{index[1]}</p>
-                <p>{index[2]}</p>
+                <p>{request.firstShift_id}</p>
+                <p>TIME NOT FOUND</p>
               </div>
             </div>
             <div className={requestContentStyles}>
               <p>Yours:</p>
               <div className="flex flex-col text-right">
-                <p>{index[3]}</p>
-                <p>{index[4]}</p>
+                <p>{request.secondShift_id}</p>
+                <p>TIME NOT FOUND</p>
               </div>
             </div>
           </div>
