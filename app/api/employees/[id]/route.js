768657846619fbfbe5451de5ce25168aac723cb4
Gabriel Leclerc <gleclerc.yk1@gmail.com>
2024-04-04 17:01:56 -0600

    main page logic

--- app/api/employees/[id]/route.js
+++ app/api/employees/[id]/route.js
@@ -4,9 +4,9 @@ import { NextResponse } from "next/server";
 
 export async function PUT(request, { params }) {
     const { id } = params;
-    const { newFirstName: firstName, newLastName: lastName, newDepartment_id: department_id, newIsManager: isManager } = await request.json();
+    const { newFirstName: firstName, newLastName: lastName, newEmail: email, newDepartment_id: department_id, newIsManager: isManager } = await request.json();
     await connectMongoDB();
-    await Employee.findByIdAndUpdate(id, { firstName, lastName, department_id, isManager });
+    await Employee.findByIdAndUpdate(id, { firstName, lastName, email, department_id, isManager });
     return NextResponse.json({ message: "Employee updated" }, { status: 200 });
 }
 
--- components/ShiftsList.tsx
+++ components/ShiftsList.tsx
@@ -1,7 +1,8 @@
 'use client'
-import { getShifts } from "@/libs/dbAccess";
+import { getEmployees, getShifts, getDepartments } from "@/libs/dbAccess";
 import { useState, useEffect } from "react";
 import ShiftRequest from "./ShiftRequest";
+import { set } from "mongoose";
 
 interface Shift {
   startDate: string;
@@ -12,22 +13,47 @@ interface Shift {
   _id: string;
 }
 
+interface Employee {
+  firstName: String,
+  lastName: String,
+  email: String,
+  department_id: String,
+  isManager: Boolean,
+  _id: String
+}
 
+interface Department {
+  department: string,
+  _id: String
+}
 
-
-export default function ShiftList() {
-
-  
+export default function ShiftList(params: { email: any }) {
+  console.log(params.email);
   const shiftStyle = "flex flex row justify-between";
-  const [shifts, setShifts] = useState<Shift[]>([]);
   const [modalState, setModalState] = useState(-1);
+  const [shifts, setShifts] = useState<Shift[]>([]);
+  const [employees, setEmployees] = useState<Employee[]>([]);
+  const [departments, setDepartments] = useState<Department[]>([]);
 
   useEffect(() => {
     async function fetchShifts() {
       try {
+        let employee_id = "";
         const shiftsData = await getShifts();
+        // get all employees and departments
+        let employees = await getEmployees();
+        setEmployees(employees);
+        let departments = await getDepartments();
+        setDepartments(departments);
+        // find employee by matching the email
+        for (let i = 0; i < employees.length; i++) {
+          if (employees[i].email == params.email) {
+            employee_id = employees[i]._id;
+          }
+        }
+
         // the user "65fb91a6733b75969216cb2d" is being used for testing purposes
-        const userShifts = shiftsData.filter((shift: Shift) => shift.employee_id === "65fb91a6733b75969216cb2d");
+        const userShifts = shiftsData.filter((shift: Shift) => shift.employee_id == employee_id);
 
         setShifts(userShifts);
       } catch (error) {
@@ -35,11 +61,31 @@ export default function ShiftList() {
       }
     }
     fetchShifts();
-  }, []);
+  }, [params.email]);
 
   const toggleModal = (key: number) => {
     setModalState((previous) => (previous === key ? -1 : key));
   };
+
+  function getEmployeeName(employee_id: string) {
+    let employeeName = "";
+    for (let i = 0; i < employees.length; i++) {
+      if (employees[i]._id == employee_id) {
+        employeeName = employees[i].firstName + " " + employees[i].lastName;
+      }
+    }
+    return employeeName;
+  }
+
+  function getDepartmentName(department_id: string) {
+    let departmentName = "";
+    for (let i = 0; i < departments.length; i++) {
+      if (departments[i]._id == department_id) {
+        departmentName = departments[i].department;
+      }
+    }
+    return departmentName;
+  }
 
   return (
     <>
@@ -57,11 +103,11 @@ export default function ShiftList() {
               </div>
               <div className={shiftStyle}>
                 <h2>Employee:</h2>
-                <p>{shift.employee_id}</p>
+                <p>{getEmployeeName(shift.employee_id)}</p>
               </div>
               <div className={shiftStyle}>
                 <h2>Department:</h2>
-                <p>{shift.department_id}</p>
+                <p>{getDepartmentName(shift.department_id)}</p>
               </div>
               <div className={shiftStyle}>
                 <h2>Status:</h2>
