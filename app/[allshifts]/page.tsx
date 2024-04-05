ad468005d6346873863f2cc66fa592f4cd4dd124
Gabriel Leclerc <gleclerc.yk1@gmail.com>
2024-04-04 18:48:54 -0600

    Allshifts page backend logic

--- app/[allshifts]/page.tsx
+++ app/[allshifts]/page.tsx
@@ -7,6 +7,7 @@ import ShiftRequest from "@/components/ShiftRequest";
 import { usePathname } from "next/navigation";
 import { useRouter } from "next/navigation";
 import { useSession } from "next-auth/react";
+import { getEmployees, getDepartments } from "@/libs/dbAccess";
 
 // STYLE CONSTANTS
 const shiftStyle = "flex flex row justify-between";
@@ -20,43 +21,79 @@ interface Shift {
   _id: string;
 }
 
+interface Employee {
+  firstName: String,
+  lastName: String,
+  email: String,
+  department_id: String,
+  isManager: Boolean,
+  _id: String
+}
+
+interface Department {
+  department: string,
+  _id: String
+}
+
 export default function AllShifts() {
+  const router = useRouter();
+  const { status, data: session } = useSession();
+  const userEmail = session?.user?.email;
   const [modalState, setModalState] = useState(-1);
   const [shifts, setShifts] = useState<Shift[]>([]);
-
-  const shiftIdImporter = () => {
-
-    //Import URL Pathname including Perameters
-
-    const pathname = usePathname();
-
-    const shiftId = pathname.split(":")[1];
-
-    //ShiftID Debug: console.log(shiftId);
-
-
-
-    //Sanitize and Verify ShiftId is Valid
-
-    shiftIdCheck();
-  }
+  const [employees, setEmployees] = useState<Employee[]>([]);
+  const [departments, setDepartments] = useState<Department[]>([]);
 
   const toggleModal = (key: number) => {
     setModalState((previous) => (previous === key ? -1 : key));
   };
+  
   useEffect(() => {
     async function fetchShifts() {
       try {
+        let employee_id = "";
         const shiftsData = await getShifts();
-        setShifts(shiftsData);
+        // get all employee and departments
+        let employees = await getEmployees();
+        setEmployees(employees);
+        let departments = await getDepartments();
+        setDepartments(departments);
+        // find employee by matching the email
+        for (let i = 0; i < employees.length; i++) {
+          if (employees[i].email == userEmail) {
+              employee_id = employees[i]._id;
+          }
+        }
+
+        const availableShifts = shiftsData.filter((shift: Shift) => shift.employee_id != employee_id);
+
+        setShifts(availableShifts);
       } catch (error) {
         console.error("Error fetching shifts:", error);
       }
     }
     fetchShifts();
-  }, []);
-  const router = useRouter();
-  const { status, data: session } = useSession();
+  }, [userEmail]);
+
+  function getEmployeeName(employee_id: string) {
+    let employeeName = "";
+    for (let i = 0; i < employees.length; i++) {
+      if (employees[i]._id == employee_id) {
+        employeeName = employees[i].firstName + " " + employees[i].lastName;
+      }
+    }
+    return employeeName;
+  }
+
+  function getDepartmentName(department_id: string) {
+    let departmentName = "";
+    for (let i = 0; i < departments.length; i++) {
+      if (departments[i]._id == department_id) {
+        departmentName = departments[i].department;
+      }
+    }
+    return departmentName;
+  }
 
   // If the user is not authenticated get routed to main home page
   if (status === "unauthenticated"){
@@ -83,11 +120,11 @@ export default function AllShifts() {
                 </div>
                 <div className={shiftStyle}>
                   <h2>Employee:</h2>
-                  <p>{shift.employee_id}</p>
+                  <p>{getEmployeeName(shift.employee_id)}</p>
                 </div>
                 <div className={shiftStyle}>
                   <h2>Department:</h2>
-                  <p>{shift.department_id}</p>
+                  <p>{getDepartmentName(shift.department_id)}</p>
                 </div>
                 <div className={shiftStyle}>
                   <h2>Status:</h2>
