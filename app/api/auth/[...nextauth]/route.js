11c5f52c69cc49094e8554f57d6d7365fb473895
RCC-STUDIO <rccstudio.clarksor@gmail.com>
2024-04-10 17:32:01 -0600

    Update Files

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,5 +1,5 @@
-import NextAuth from 'next-auth';
-import GoogleProvider from 'next-auth/providers/google';
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
 export const authOptions = {
   providers: [
     GoogleProvider({
@@ -11,4 +11,18 @@ export const authOptions = {
 const handler = NextAuth(authOptions);
 export { handler as GET, handler as POST };
 
-
+/*
+import NextAuth from 'next-auth';
+import Providers from 'next-auth/providers';
+//import GoogleProvider from 'next-auth/providers/google';
+export const authOptions = {
+  providers: [
+    Providers.Google ({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+  ],
+};
+const handler = NextAuth(authOptions);
+export { handler as GET, handler as POST };
+*/
