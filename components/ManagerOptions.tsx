d73c20bd456bdd981f981523d20f86fa8834ab41
Gabriel Leclerc <gleclerc.yk1@gmail.com>
2024-04-11 17:21:06 -0600

    Manager Options Dropped and Swapped

--- components/ManagerOptions.tsx
+++ components/ManagerOptions.tsx
@@ -24,32 +24,42 @@ interface Shift {
 
 const ManagerOptions = ({ request }: { request: Request}) => {
   const handleAccept = async () => {
-    // CHANGE THE STATUS OF THE REQUEST TO ACCEPTED
-    const newRequest = {
-      newFirst_shift_id: request.first_shift_id,
-      newSecond_shift_id: request.second_shift_id,
-      newRequester_employee_id: request.requester_employee_id,
-      newRequestee_employee_id: request.requestee_employee_id,
-      newDepartment_id: request.department_id,
-      newStatus: "swapped"
+    if (request.second_shift_id != null) {
+      // CHANGE THE STATUS OF THE REQUEST TO ACCEPTED
+      const newRequest = {
+        newFirst_shift_id: request.first_shift_id,
+        newSecond_shift_id: request.second_shift_id,
+        newRequester_employee_id: request.requester_employee_id,
+        newRequestee_employee_id: request.requestee_employee_id,
+        newDepartment_id: request.department_id,
+        newStatus: "swapped"
+      }
+      updateRequest(request._id, newRequest);
+      
+      const shifts = await getShifts();
+      const firstShift = shifts.find((shift: Shift) => shift._id == request.first_shift_id);
+      const secondShift = shifts.find((shift: Shift) => shift._id == request.second_shift_id);
+      // change the employee_id of the first shift to the requestee_employee_id
+      const newFirstShift = {
+        newEmployee_id: request.requestee_employee_id,
+        newStatus: "filled"
+      }
+      // change the employee_id of the second shift to the requester_employee_id
+      const newSecondShift = {
+        newEmployee_id: request.requester_employee_id,
+        newStatus: "filled"
+      }
+      updateShift(request.first_shift_id, newFirstShift);
+      updateShift(request.second_shift_id, newSecondShift);
+    } else if (request.second_shift_id == null) {
+      const newRequest = {
+        newStatus: "approved"
+      }
+      updateRequest(request._id, newRequest);
+
+      const shifts = await getShifts();
+      const firstShift = shifts.find((shift: Shift) => shift._id == request.first_shift_id);
     }
-    updateRequest(request._id, newRequest);
-    
-    const shifts = await getShifts();
-    const firstShift = shifts.find((shift: Shift) => shift._id == request.first_shift_id);
-    const secondShift = shifts.find((shift: Shift) => shift._id == request.second_shift_id);
-    // change the employee_id of the first shift to the requestee_employee_id
-    const newFirstShift = {
-      newEmployee_id: request.requestee_employee_id,
-      newStatus: "filled"
-    }
-    // change the employee_id of the second shift to the requester_employee_id
-    const newSecondShift = {
-      newEmployee_id: request.requester_employee_id,
-      newStatus: "filled"
-    }
-    updateShift(request.first_shift_id, newFirstShift);
-    updateShift(request.second_shift_id, newSecondShift);
   }
 
   const handleDeny = () => {
