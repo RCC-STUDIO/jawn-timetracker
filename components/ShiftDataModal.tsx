277ff060cb1c7c97291863b055499bac4550fbc7
Matt <147024601+matthew-simms-298@users.noreply.github.com>
2024-04-11 11:43:51 -0600

    Update ShiftDataModal.tsx

--- components/ShiftDataModal.tsx
+++ components/ShiftDataModal.tsx
@@ -15,7 +15,7 @@ interface Shift {
 
 export default function ShiftDataModal({shiftData, deptName, employeeName}: { shiftData: Shift, deptName: string, employeeName: string }) {
     return (
-        <div className="p-4 m-2 mb-12 border border-blue-950 rounded-md bg-blue-950">
+        <div className="p-4 m-2 mb-12 border border-blue-950 rounded-md bg-blue-950 mx-auto">
             <h1 className="text-lg font-bold underline">DATE: {new Date(shiftData.startDate).toLocaleDateString()}</h1>
             <p>{new Date(shiftData.startDate).toLocaleTimeString()} - {new Date(shiftData.endDate).toLocaleTimeString()}</p>
             <p>Department: {deptName}</p>
