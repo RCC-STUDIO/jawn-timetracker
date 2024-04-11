a756707ce3fa59861d14fb9b297f4cc3771933b1
Matt <147024601+matthew-simms-298@users.noreply.github.com>
2024-04-11 12:22:17 -0600

    fixing modal errors

--- components/ShiftDataModal.tsx
+++ components/ShiftDataModal.tsx
@@ -1,25 +1,36 @@
-'use client'
+"use client";
 
 import Shift from "@/models/shift";
-import React from "react"
+import React from "react";
 
 interface Shift {
-    startDate: Date;
-    endDate: Date;
-    employee_id: string;
-    department_id: string;
-    status: string;
-    _id: string;
-  }
+  startDate: Date;
+  endDate: Date;
+  employee_id: string;
+  department_id: string;
+  status: string;
+  _id: string;
+}
 
-
-export default function ShiftDataModal({shiftData, deptName, employeeName}: { shiftData: Shift, deptName: string, employeeName: string }) {
-    return (
-        <div className="fixed inset-0 flex items-center justify-center bg-blue-950 bg-opacity-50">
-        <h1 className="text-lg font-bold underline">DATE: {new Date(shiftData.startDate).toLocaleDateString()}</h1>
-            <p>{new Date(shiftData.startDate).toLocaleTimeString()} - {new Date(shiftData.endDate).toLocaleTimeString()}</p>
-            <p>Department: {deptName}</p>
-            <p>Employee: {employeeName}</p>
-        </div>
-    )
+export default function ShiftDataModal({
+  shiftData,
+  deptName,
+  employeeName,
+}: {
+  shiftData: Shift;
+  deptName: string;
+  employeeName: string;
+}) {
+  return (
+      <div className=" flex flex-col text-white bg-blue-950 p-8">
+        <h1 className="text-lg font-bold underline">
+          DATE: {new Date(shiftData.startDate).toLocaleDateString()}
+        </h1>
+        <p>
+          {new Date(shiftData.startDate).toLocaleTimeString()} - {new Date(shiftData.endDate).toLocaleTimeString()}
+        </p>
+        <p>Department: {deptName}</p>
+        <p>Employee: {employeeName}</p>
+      </div>
+  );
 }
